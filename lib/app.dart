ba10d65e95287992b83f7a7d8c5b585a3e44f0f9
OmarHisham99 <ohisham413@gmail.com>
2021-10-02 06:17:10 +0200

    Ui latest update 10/2/2021

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:rosheta/welcome_page.dart';
 import 'Screens/login.dart';
 class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        home:Login()
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home:welcome_page()
     );
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'Screens/login.dart';
 import 'app.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  //WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
+
   runApp(App());
 }
